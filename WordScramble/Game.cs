6b4f484d0b1c1e8240b14209a63736419cf6abec
MiguelPereira-a22411261 <miguel.pereirawork08@gmail.com>
2025-04-11 17:00:49 +0100

    Add missing code for method ShowMenu

--- WordScramble/Game.cs
+++ WordScramble/Game.cs
@@ -52,11 +52,10 @@ namespace WordScramble
                 switch (choice)
                 {
                     case "Start Game":
-
-                        // ////////// => TO IMPLEMENT <= //////////// //
+                        StartGame();
                         break;
                     case "View Game Stats":
-                        // ////////// => TO IMPLEMENT <= //////////// //
+                        ShowGameStats();
                         break;
                     case "Quit":
                         return;
